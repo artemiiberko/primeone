b0fd63f64a0d63a482229e34137bf49a86006560
artemiiberko <artemii.berko@gmail.com>
2021-05-08 17:39:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+//Adaptive functions
+$(document).ready(function () {
+    $('.header-menu__burger').click(function (event) {
+        $('.header-menu__burger,.header-menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+})
+
+function ibg() {
+    $.each($('.ibg'), function (index, val) {
+        if ($(this).find('img').length>0){ 
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    })
+}
+ibg();
